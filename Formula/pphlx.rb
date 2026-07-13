37340eaff2daf46fcfb1985a3ca8ed441a60884e
MIKETYSON <miketyson@duck.com>
2026-07-14 00:37:32 +0530

    chore: Update formula with final release SHA256 checksums

--- Formula/pphlx.rb
+++ Formula/pphlx.rb
@@ -7,18 +7,18 @@ class Pphlx < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-darwin-arm64.tar.gz"
-      sha256 "47b367edf00214f3a9d96fe9c9d1a463e46753769fc5e8aaf81a5747dedb2578"
+      sha256 "a43aaab7b942c176e2e8f28d1ada9b3f9808f602e364002df167abf7c0b93fa7"
     else
       url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-darwin-amd64.tar.gz"
-      sha256 "0e178a9886af5448aac8c7882b83d608834f35c7e5f5f104e2f8ed6737906a74"
+      sha256 "e9f12879637ded5b4c36c118c2c43fc33902e1d5dfec8cc95316553bb4430859"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-linux-arm64.tar.gz"
-      sha256 "d119908a9cdbf24f0ca6f9b06757827630b870f6000c2c572b6a449fc7b92aaa"
+      sha256 "bda08bc11f128e1f7c62998265464235ed9512ffa2fcd0b8be68d114eb7e14dd"
     else
       url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-linux-amd64.tar.gz"
-      sha256 "0f73eea015a495eca621dd14e281f962d6acc3e292a81f3b23dcecacad7d4b29"
+      sha256 "6ab5b601975edc0c26aacabc130855453f9b5262c877ab1b0b506fcf43fca867"
     end
   end
 
