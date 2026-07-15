7112dbc1416b0bd13b56afcd18171a9ecbf9e653
MIKETYSON <miketyson@duck.com>
2026-07-15 15:36:59 +0530

    feat(brew): Update Formula to version 1.0.4

--- Formula/pphlx.rb
+++ Formula/pphlx.rb
@@ -1,24 +1,24 @@
 class Pphlx < Formula
   desc "High-performance component compiler that compiles modern web components to pure PHP"
   homepage "https://pphlx.org"
-  version "1.0.0"
+  version "1.0.4"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-darwin-arm64.tar.gz"
-      sha256 "a43aaab7b942c176e2e8f28d1ada9b3f9808f602e364002df167abf7c0b93fa7"
+      url "https://github.com/pphlx/pphlx/releases/download/v1.0.4/pphlx-darwin-arm64.tar.gz"
+      sha256 "4880e386fb1fe5860a92170136cc5d56b81b32cc1c4af3e93fa3cda3444358d6"
     else
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-darwin-amd64.tar.gz"
-      sha256 "e9f12879637ded5b4c36c118c2c43fc33902e1d5dfec8cc95316553bb4430859"
+      url "https://github.com/pphlx/pphlx/releases/download/v1.0.4/pphlx-darwin-amd64.tar.gz"
+      sha256 "68341a2d2243da3beb05625a094452e827b6e84e79825061f4bb0cb80d340862"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-linux-arm64.tar.gz"
-      sha256 "bda08bc11f128e1f7c62998265464235ed9512ffa2fcd0b8be68d114eb7e14dd"
+      url "https://github.com/pphlx/pphlx/releases/download/v1.0.4/pphlx-linux-arm64.tar.gz"
+      sha256 "0fe2eff78009715b3fce7d78a1d0e0b21f5cf54492e751609da023c332ed7c79"
     else
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.0/pphlx-linux-amd64.tar.gz"
-      sha256 "6ab5b601975edc0c26aacabc130855453f9b5262c877ab1b0b506fcf43fca867"
+      url "https://github.com/pphlx/pphlx/releases/download/v1.0.4/pphlx-linux-amd64.tar.gz"
+      sha256 "ceb2a937b1e9eb680df04d837ee661c09ea425d4166a8e803eba34750adbaae7"
     end
   end
 
