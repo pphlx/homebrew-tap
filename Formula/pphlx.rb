685f0599d5242a33da16ec1fd677660b382b0c02
MIKETYSON <miketyson@duck.com>
2026-07-23 23:17:57 +0530

    feat(release): Update Formula for PPHLX v1.1.0

--- Formula/pphlx.rb
+++ Formula/pphlx.rb
@@ -1,29 +1,45 @@
 class Pphlx < Formula
-  desc "High-performance component compiler that compiles modern web components to pure PHP"
-  homepage "https://pphlx.org"
-  version "1.0.9"
+  desc "PPHLX Compiler CLI for PHP Component-Driven Full-Stack Web Development"
+  homepage "https://pphlx.dev"
+  version "1.1.0"
   license "MIT"
 
-  if OS.mac?
+  on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.9/pphlx-darwin-arm64.tar.gz"
-      sha256 "3574f3761bad5fd5030f023774cb6756cc1ddab5a5f45c2210e2f38d11564c2d"
-    else
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.9/pphlx-darwin-amd64.tar.gz"
-      sha256 "850c376d008f03be780a7d7e12100754a4d2c57c1eb7dea15bf4f0f17dcde662"
+      url "https://github.com/pphlx/pphlx/releases/download/v1.1.0/pphlx-darwin-arm64.tar.gz"
+      sha256 "66fe53524f0a3f3f5c5551c0e059a74ad0879192c63c969a224ad9259218c1af"
+
+      def install
+        bin.install "pphlx-macos-arm64" => "pphlx"
+      end
     end
-  elsif OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.9/pphlx-linux-arm64.tar.gz"
-      sha256 "9af8b525e970582824f3032f19aca46f4531cf8ab03c3dd4230feec7e5633e7f"
-    else
-      url "https://github.com/pphlx/pphlx/releases/download/v1.0.9/pphlx-linux-amd64.tar.gz"
-      sha256 "376fafab4f26ee190cb008939495528aceecf30565941fcb8c9b474f4ba5b00d"
+    if Hardware::CPU.intel?
+      url "https://github.com/pphlx/pphlx/releases/download/v1.1.0/pphlx-darwin-amd64.tar.gz"
+      sha256 "13173a68a36c8ef379398649f60b9335898f0f04f342f6cb07a7c24b0d6279c5"
+
+      def install
+        bin.install "pphlx-macos-amd64" => "pphlx"
+      end
     end
   end
 
-  def install
-    bin.install "pphlx"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/pphlx/pphlx/releases/download/v1.1.0/pphlx-linux-arm64.tar.gz"
+      sha256 "58956587a0c978c6fbf44c27f3a11377d9cbba88731ca84926524ec872bc92a6"
+
+      def install
+        bin.install "pphlx-linux-arm64" => "pphlx"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/pphlx/pphlx/releases/download/v1.1.0/pphlx-linux-amd64.tar.gz"
+      sha256 "d2886f718250ca6f63223c7a59ab13f582913337c35dfe67a52d14f1c5ce390b"
+
+      def install
+        bin.install "pphlx-linux-amd64" => "pphlx"
+      end
+    end
   end
 
   test do
